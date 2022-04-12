a7c8604bc500b01b716c414ead9ea483d7450a94
mtvkand <mtvkand@gmail.com>
2022-04-12 21:43:42 +0300

    - Версия библиотеки long снижена до 4.0.0 из-за проблем с компиляцией @grpc/proto-ts - Изменена команда для генерации прото файлов. Добавлена опция esModuleInterop

--- generateProto.sh
+++ generateProto.sh
@@ -12,7 +12,7 @@ mkdir ./src/generated/
 
 chmod 755 ./src/generated
 
-./node_modules/.bin/grpc_tools_node_protoc --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_out=./src/generated --ts_proto_opt=outputServices=generic-definitions,useExactTypes=false -I ./protos/ ./protos/*.proto
+./node_modules/.bin/grpc_tools_node_protoc --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts_proto --ts_proto_opt=esModuleInterop=true --ts_proto_out=./src/generated --ts_proto_opt=outputServices=generic-definitions,useExactTypes=false -I ./protos/ ./protos/*.proto
 
 if [ $? -eq 0 ]
 then
